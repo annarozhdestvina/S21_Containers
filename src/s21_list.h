7786be778de82750cf096b9895eda3d64b339171
MikhailPimenov <misha4git@yandex.ru>
2023-05-14 12:19:21 +0300

    develop/iterator is in progress

--- src/s21_list.h
+++ src/s21_list.h
@@ -1,36 +1,124 @@
 #ifndef _S21_LIST_H_
 #define _S21_LIST_H_
 
+#include <new>  // new
+
 namespace s21 {
 
-    class SequentionalBase {
+    template <typename List>
+    class ListIterator {
+    private:
+        using Value_type = typename List::value_type;
+        using Reference = typename List::reference;
+        using Const_reference = typename List::const_reference;
+        using Pointer = typename List::pointer;
+        using Const_pointer = typename List::const_pointer;
+        using Node_type = typename List::Node;
+        using Node_pointer = Node_type*;
+        using Node_reference = Node_type&;
+    
     public:
-        virtual void Push_back() = 0;
-        virtual void Pop_back() = 0;
+        ListIterator(Node_pointer pointer) :  pointer_(pointer) {};
+
+        ListIterator& operator++() {
+            // TODO: check if the next node exists?
+            pointer_ = pointer_->next_;
+            return *this;
+        }
+
+        ListIterator operator++(int) {
+            ListIterator temporary(*this);
+            // TODO: check if the next node exists?
+            pointer_ = pointer_->next_;
+            return temporary;
+        }
+
+        ListIterator& operator--() {
+            // TODO: check if the previous node exists?
+            pointer_ = pointer_->previous_;
+            return *this;
+        }
+
+        ListIterator operator--(int) {
+            ListIterator temporary(*this);
+            // TODO: check if the previous node exists?
+            pointer_ = pointer_->previous_;
+            return temporary;
+        }
+
+        Reference operator*() {
+            return pointer_->data_;
+        }
+
+        Const_reference operator*() const{
+            return pointer_->data_;
+        }
+
+        Pointer operator->() {
+            return &(pointer_->data_);
+        }
+
+        Const_pointer operator->() const {
+            return &(pointer_->data_);
+        }
+
+    private:
+        Node_pointer pointer_;
     };
 
-    using Type = double;
 
     template <typename Type>
-    // class List : public SequentionalBase<Type> {
-    // class List : public SequentionalBase {
     class List {
+        friend class ListIterator<List<Type>>;
     private:
         using value_type = Type;
-        // using size_type = std::size_t;
-        using size_type = int;
-        // using reference = value_type&;
+        // allocator_type	Allocator
+        using size_type = std::size_t;  //	Unsigned integer type (usually std::size_t)
+        // difference_type	Signed integer type (usually std::ptrdiff_t)
+        using reference = value_type&;
+        using const_reference = const value_type&;
+        using pointer = value_type*; 
+        // Allocator::pointer	(until C++11)
+        // std::allocator_traits<Allocator>::pointer	(since C++11)
+        
+        using const_pointer = const value_type*;	 
+        // Allocator::const_pointer	(until C++11)
+        // std::allocator_traits<Allocator>::const_pointer	(since C++11)
+        
+        using iterator = ListIterator<List<Type>>;
+        // iterator	LegacyBidirectionalIterator to value_type
+        // const_iterator	LegacyBidirectionalIterator to const value_type
+        // reverse_iterator	std::reverse_iterator<iterator>
+        // const_reverse_iterator	std::reverse_iterator<const_iterator>
+
 
     private:
-        value_type data_;
-        List* next_ = nullptr;
-        List* previous_ = nullptr;
+        class Node {
+            value_type data_;
+            List* next_ = nullptr;
+            List* previous_ = nullptr;
+        } node_;
 
+        size_type size_ = 0;
+
+        List* begin_ = nullptr;
+        List* end_ = nullptr;
+        
     public:
-        List() = default;
+        List() {};
 
 
+        size_type Size() const {
+            return size_;
+        }
 
+        iterator begin() {
+            return iterator(&(begin_->data_));
+        }
+
+        iterator cbegin() const {
+            return iterator(&(begin_->data_));
+        }
     };
 
 }  // namespace s21
--- src/tests/list/list.cc
+++ src/tests/list/list.cc
@@ -2,10 +2,38 @@
 #include <list>
 #include "../../s21_list.h"
 
-namespace {
+// GCOVR_EXCL_START
 
-    TEST(ListTests, Test0) {
-        s21::List<double> s21_list;
-        EXPECT_TRUE(true);
+template <typename Type>
+bool operator==(const s21::List<Type>& s21_list, const std::list<Type>& list) {
+    if (s21_list.Size() != list.size())
+        return false;
+
+    // const auto iterator = list.cbegin();
+    // const auto s21_iterator = s21_list.cbegin();
+
+    // while (iterator != list.cend()) {
+    //     if (*iterator != *s21_iterator)
+    //         return false;
+    //     ++iterator;
+    //     ++s21_iterator;
+    // }    
+    
+    return true;
+}
+
+class Item {
+
+};
+
+namespace {
+    TEST(ListTests, Test0DefaultConstructor) {
+        s21::List<Item> s21_list;
+        std::list<Item> list;
+
+
+        EXPECT_TRUE(s21_list == list);
     }
 }   //  namespace
+
+// GCOVR_EXCL_STOP
