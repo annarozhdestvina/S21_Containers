27f2dacaa118bb4e72a109d3519843aa995981cd
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-08-16 03:14:10 +0300

    map/expand insert tests with negative keys

--- src/tests/map/modifiers.cc
+++ src/tests/map/modifiers.cc
@@ -59,6 +59,54 @@ TEST(Map, T2Insert)
 }
 
 
+TEST(Map, T3Insert)
+{
+    s21::Map<int, Item> s21_map;
+    s21_map.Insert(std::make_pair(-4, Item(4)));
+    s21_map.Insert(std::make_pair(6, Item(6)));
+    s21_map.Insert(std::make_pair(7, Item(7)));
+    s21_map.Insert(std::make_pair(1, Item(1)));
+
+    std::cout << s21_map << '\n';
+
+    // s21::Map<>::const_iterator s21_it = s21_map.cbegin();
+
+    std::map<int, Item> map;
+
+    // EXPECT_EQ(s21_map, map);
+}
+
+TEST(Map, T4Insert)
+{
+    s21::Map<int, Item> s21_map;
+    s21_map.Insert(std::make_pair(-4, Item(4)));
+    s21_map.Insert(std::make_pair(-6, Item(6)));
+    s21_map.Insert(std::make_pair(-7, Item(7)));
+    s21_map.Insert(std::make_pair(-1, Item(1)));
+
+    std::cout << s21_map << '\n';
+
+    std::map<int, Item> map;
+
+    // EXPECT_EQ(s21_map, map);
+}
+
+TEST(Map, T5Insert)
+{
+    s21::Map<int, Item> s21_map;
+    s21_map.Insert(std::make_pair(-4, Item(4)));
+    s21_map.Insert(std::make_pair(6, Item(6)));
+    s21_map.Insert(std::make_pair(0, Item(7)));
+    s21_map.Insert(std::make_pair(-1, Item(1)));
+
+    std::cout << s21_map << '\n';
+
+    std::map<int, Item> map;
+
+    // EXPECT_EQ(s21_map, map);
+}
+
+
 
 
 
