030845fcfe2176eaa6e437b21d7e82da45bb285e
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-08-24 20:50:21 +0300

    map/erase test

---
+++ src/tests/map/erase.cc
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+
+#include <stack>
+#include <list>
+#include <vector>
+#include <deque>
+#include <map>
+
+#include "../../s21_map.h"
+#include "../item.h"
+#include "../map_helpers.h"
+
+// GCOVR_EXCL_START
+
+namespace
+{
+
+
+TEST(Map, T0Erase)
+{
+    s21::Map<int, Item> s21_map;
+    std::map<int, Item> map;
+
+    s21_map.Insert(std::make_pair(4, Item()));
+    s21_map.Insert(std::make_pair(3, Item()));
+    s21_map.Insert(std::make_pair(7, Item()));
+
+    std::cout << s21_map << '\n';
+
+    map.insert(std::make_pair(4, Item()));
+    map.insert(std::make_pair(3, Item()));
+    map.insert(std::make_pair(7, Item()));
+    std::cout << "Before erase: \n";
+    for (auto it = map.cbegin(); it != map.cend(); ++it)
+        std::cout << "{" << it->first << " : " << it->second << "}\n";
+
+
+    map.erase(7);
+    s21_map.Erase(7, Item());
+    std::cout << "After erase: \n\n";
+
+    // for (auto it = map.cbegin(); it != map.cend(); ++it)
+    //     std::cout << "{" << it->first << " : " << it->second << "}\n";
+
+    // EXPECT_EQ(s21_map, map);
+}
+
+TEST(Map, T1Erase)
+{
+    s21::Map<int, Item> s21_map;
+    s21_map.Insert(std::make_pair(4, Item()));
+    s21_map.Insert(std::make_pair(3, Item()));
+    s21_map.Insert(std::make_pair(5, Item()));
+
+    // EXPECT_EQ(s21_map, map);
+}
+
+TEST(Map, T2Erase)
+{
+    s21::Map<int, Item> s21_map;
+    s21_map.Insert(std::make_pair(4, Item()));
+
+    
+}
+
+
+
+
+
+
+
+}  // namespace
+
+// GCOVR_EXCL_STOP
