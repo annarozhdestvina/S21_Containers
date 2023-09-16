98b68de3c335dd184eec063c5d9dd3aa1ee81692
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-16 21:28:16 +0300

    other in progress

--- src/tests/array/other.cc
+++ src/tests/array/other.cc
@@ -16,8 +16,8 @@ TEST(Vector, T0OperatorEqual)
     s21::Array<Item, 4> s21_source { Item(11), Item(22, 'b'), Item(33, 'c', 0.3333), Item(44, 'd', 0.44444) };
     std::array<Item, 4> source { Item(11), Item(22, 'b'), Item(33, 'c', 0.3333), Item(44, 'd', 0.44444) };
     
-    s21::Array<Item, 4> s21_vector(s21_source.begin(), s21_source.end());
-    std::array<Item, 4> vector(source.begin(), source.end());
+    s21::Array<Item, 2> s21_vector(s21_source.begin(), s21_source.end());
+    std::array<Item, 2> vector(source.begin(), source.end());
 
     EXPECT_TRUE(s21_vector == vector);
 }   
@@ -27,7 +27,7 @@ TEST(Vector, T1OperatorEqual)
     s21::Array<Item, 4> s21_source { Item(11), Item(22, 'b'), Item(33, 'c', 0.3333), Item(44, 'd', 0.44444) };
     std::array<Item, 4> source { Item(11), Item(22, 'b'), Item(33, 'c', 0.3333), Item(44, 'd', 0.44444), Item(55, 'e', 0.55555555) };
     
-    s21::Array<Item, 4> s21_vector(s21_source.begin(), s21_source.end());
+    s21::Array<Item, 0> s21_vector(s21_source.begin(), s21_source.end());
     std::array<Item, 4> vector(source.begin(), source.end());
 
     EXPECT_FALSE(s21_vector == vector);
@@ -106,8 +106,8 @@ TEST(Vector, T8OperatorNotEqual)
     s21::Array<Item, 4> s21_source { Item(11), Item(22, 'b'), Item(33, 'c', 0.3333), Item(44, 'd', 0.44444) };
     s21::Array<Item, 4> source { Item(6666), Item(22, 'b'), Item(33, 'c', 0.3333), Item(44, 'd', 0.44444) };
     
-    s21::Array<Item, 4> s21_vector(s21_source.begin(), s21_source.end());
-    s21::Array<Item, 4> vector(source.begin(), source.end());
+    s21_vector= s21_source.begin();//, s21_source.end());
+     vector= source.begin();//, source.end());
 
     EXPECT_TRUE(s21_vector != vector);
 }   
